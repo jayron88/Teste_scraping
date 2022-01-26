65a8ef75e418f4dfa6ea08fe411f8a623de8f041
jayron88 <jayron@hotmail.com.br>
2022-01-26 17:29:00 -0300

    Update json_to_csv.py

--- json_to_csv.py
+++ json_to_csv.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-df = pd.read_json ('trf/trf/spiders/refeito.json')
+df = pd.read_json ('trf/trf/spiders/scrap.json')
 df.to_csv (r'C:/Users/{}/Desktop/{}.csv', index = None) #nesta parte você coloca o path onde deseja salvar o .csv convertido de json
